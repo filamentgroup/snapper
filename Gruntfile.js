c177083e2438617d341a2934165cf0d4da4b26ec
Scott Jehl <scottjehl@gmail.com>
2015-12-23 10:48:44 -0600

    update to 0.2.0 with a demo, new grunt build for sass, and better PE workflow - no more need to set widths in the markup

---
+++ Gruntfile.js
@@ -0,0 +1,54 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+	var os = require( 'os' ),
+		path = require( 'path' ),
+		isWindows = os.platform().toLowerCase().indexOf( 'win' ) === 0; // watch out for 'darwin'
+
+	require( 'matchdep' ).filterDev( 'grunt-*' ).forEach( grunt.loadNpmTasks );
+
+
+	// Project configuration.
+	grunt.initConfig({
+		pkg: '<json:package.json>',
+
+		meta: {
+			banner: '/*! snapper  - v<%= pkg.version %> - ' +
+				'<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+				'* Copyright (c) <%= grunt.template.today("yyyy") %> Filament Group */'
+		},
+
+    copy: {
+			js: {
+				files: [
+					{ expand: true, cwd: "src", src: ["*.js"], dest: "dist/" },
+          { expand: true, cwd: 'node_modules/jquery/dist', src: [ "jquery.js" ], dest: "src/lib"},
+          { expand: true, cwd: 'node_modules/fg-overthrow/src', src: [ "overthrow-toss.js" ], dest: "src/lib"}
+				]
+			}
+    },
+
+		sass: {
+      options: {
+				banner: '<%= meta.banner %>'
+			},
+			src: {
+				files: [{
+					expand: true,
+					cwd: 'src',
+					src: [ '**/*.scss' ],
+					dest: 'dist/',
+					ext: '.css'
+				}]
+			}
+		}
+
+	});
+
+
+	grunt.registerTask('default', [
+    'copy',
+		'sass'
+	]);
+
+};
