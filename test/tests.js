f1f9d6522df64994dbb6b3b12475a8e3172042f5
Scott Jehl <scottjehl@gmail.com>
2016-06-06 22:07:37 -0500

    snap test

--- test/tests.js
+++ test/tests.js
@@ -36,6 +36,18 @@ window.onload = function(){
 			ok( $(".snapper_items > *:eq(2)").attr( "style" ) !== undefined, "style inline applied" );
 			ok( $(".snapper_pane").css( "overflow" ) === "auto", "pane has overflow auto" );
 			ok( $(".snapper_pane").width() < $(".snapper_items" ).width(), "pane is narrower than items in it now" );
+			ok( $(".snapper_nextprev").length, "next prev generated" );
+			ok( $(".snapper_nextprev a").length === 2, "2 next prev links" );
+		});
+
+		asyncTest( 'Snapping occurs after scrolling to a spot that is not a snap point', function() {
+			expect(1);
+			$(".snapper_pane")[0].scrollLeft = 0;
+			$(".snapper_pane")[0].scrollLeft = 35;
+			setTimeout(function(){
+				ok( $(".snapper_pane")[0].scrollLeft ===0 );
+				start();
+			},1000);
 		});
 
 
