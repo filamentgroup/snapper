8a2461754494b1ac64b2c0348e5708a49c8940b4
Scott Jehl <scottjehl@gmail.com>
2019-08-05 15:58:44 -0500

    x is best

--- test/tests.js
+++ test/tests.js
@@ -30,7 +30,7 @@ window.onload = function(){
 			ok($(".snapper_items > *:eq(2)").css("float", "left"));
 			ok($(".snapper_items").attr("style") !== undefined, "style inline applied");
 			ok($(".snapper_items > *:eq(2)").attr("style") !== undefined, "style inline applied");
-			ok($(".snapper_pane").css("overflow") === "auto", "pane has overflow auto");
+			ok($(".snapper_pane").css("overflow-x") === "auto", "pane has overflow auto");
 			ok($(".snapper_pane").width() < $(".snapper_items").width(), "pane is narrower than items in it now");
 			ok($(".snapper_nextprev").length, "next prev generated");
 			ok($(".snapper_nextprev a").length === 2, "2 next prev links");
